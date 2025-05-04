0b354403f9bad1e91c4295291650a9065d970b92
Maya Stein <mayarosestein@gmail.com>
2025-05-04 07:00:14 -0400

    Added method for deleting to do items on swipe

--- toDoList2/ContentView.swift
+++ toDoList2/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct ContentView: View {
     @State private var showNewTask = false
     @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack{
             HStack{
@@ -39,11 +40,17 @@ struct ContentView: View {
                     } else{
                         Text(toDoItem.title)
                     }
-                }
+                }.onDelete(perform: deleteToDo)
             }.listStyle(.plain)
         }
         if showNewTask {
             NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title:"", isImportant: false))
+        }
+    }
+    func deleteToDo(at offsets: IndexSet){
+        for offset in offsets{
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
